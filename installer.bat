092e9242a74ecb862b58a7976e4146253f07117e
ItBlake <75915011+ItBlake@users.noreply.github.com>
2020-12-13 12:51:29 -0500

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -8,10 +8,10 @@ echo.
 echo Copying files...
 echo.
 mkdir "c:\Program Files\FuckYouWEBP" > nul
-copy "startFuckYouWEBP.bat" "c:\Program Files\FuckYouWEBP" > nul
-copy "nircmd.exe" "c:\Program Files\FuckYouWEBP" > nul
-copy "convert.exe" "c:\Program Files\FuckYouWEBP" > nul
-copy "FuckYouWEBP.bat" "c:\Program Files\FuckYouWEBP" > nul
+copy /y "startFuckYouWEBP.bat" "c:\Program Files\FuckYouWEBP" > nul
+copy /y "nircmd.exe" "c:\Program Files\FuckYouWEBP" > nul
+copy /y "convert.exe" "c:\Program Files\FuckYouWEBP" > nul
+copy /y "FuckYouWEBP.bat" "c:\Program Files\FuckYouWEBP" > nul
 TIMEOUT 2 > nul
 echo Files copied!
 echo.
@@ -25,8 +25,8 @@ echo Added FuckYouWEBP to startup
 TIMEOUT 1 > nul
 echo.
 echo FuckYouWEBP Installed!
-echo.goto :startnow
-
-:startnow
+echo.
+echo Press any key to quit the installer!
+pause > nul
 start "c:\Program Files\FuckYouWEBP\startFuckYouWEBP.bat"
 exit
